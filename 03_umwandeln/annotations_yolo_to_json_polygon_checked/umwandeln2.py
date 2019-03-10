32f5e24b3a6f210100660b3a06f8083bf94141cc
PaoloSRH <35607251+PaoloSRH@users.noreply.github.com>
2019-03-10 11:34:35 +0100

    neu umwandeln2.py

--- 03_umwandeln/annotations_yolo_to_json_polygon_checked/umwandeln2.py
+++ 03_umwandeln/annotations_yolo_to_json_polygon_checked/umwandeln2.py
@@ -42,23 +42,106 @@ for file in annotations:
 		a_2 = int(round(float(word[2])*float(pic_height),0))
 		a_3 = int(round(float(word[3])*float(pic_width),0))
 		a_4 = int(round(float(word[4])*float(pic_height),0))
-		x_1 = a_1- (a_3)
-		x_2 = a_1- (a_3)
-		x_3 = a_1
-		x_4 = a_1 + (a_3)
-		x_5 = a_1 + (a_3)
-		x_6 = a_1 + (a_3)
-		x_7 = a_1
-		x_8 = a_1- (a_3)
+		if (a_1- (a_3)) >= pic_width:
+			x_1 = pic_width - 2
+		else:
+			if (a_1- (a_3)) <= 0:
+				x_1 = 1
+			else: 
+				x_1 = a_1- (a_3)
+
+		if (a_1- (a_3)) >= pic_width:
+			x_2 = pic_width - 2
+		else:
+			if (a_1- (a_3)) <= 0:
+				x_2 = 1
+			else: 
+				x_2 = a_1- (a_3)
+
 		
-		y_1 = a_2
-		y_2 = a_2 -(a_4)
-		y_3 = a_2 -(a_4)
-		y_4 = a_2 -(a_4)
-		y_5 = a_2 
-		y_6 = a_2  + a_4
-		y_7 = a_2  + a_4
-		y_8 = a_2  + a_4
+		if (a_1) >= pic_width:
+			x_3 = pic_width - 2
+		else:
+			x_3 = a_1
+
+		if (a_1 + (a_3)) >= pic_width:
+			x_4 = pic_width - 2
+		else:
+			x_4 = a_1 + (a_3)
+
+		if (a_1 + (a_3)) >= pic_width:
+			x_5 = pic_width - 2
+		else:
+			x_5 = a_1 + (a_3)
+
+		if (a_1 + (a_3)) >= pic_width:
+			x_6 = pic_width - 2
+		else:
+			x_6 = a_1 + (a_3)
+
+		if (a_1) >= pic_width:
+			x_7 = pic_width - 2
+		else:
+			x_7 = a_1
+
+		if (a_1- (a_3)) >= pic_width:
+			x_8 = pic_width - 2
+		else:
+			if (a_1- (a_3)) <= 0:
+				x_8 = 1
+			else: 
+				x_8 = a_1- (a_3)
+
+
+		if (a_2) >= pic_height:
+			y_1 = pic_height - 2
+		else:
+			y_1 = a_2
+
+		if (a_2 -(a_4)) >= pic_height:
+			y_2 = pic_height - 2
+		else:
+			if (a_2 -(a_4)) <= 0:
+				y_2 = 1
+			else: 
+				y_2 = a_2 -(a_4)
+		
+		if (a_2 -(a_4)) >= pic_height:
+			y_3 = pic_height - 2
+		else:
+			if (a_2 -(a_4)) <= 0:
+				y_3 = 1
+			else: 
+				y_3 = a_2 -(a_4)
+
+		if (a_2 -(a_4)) >= pic_height:
+			y_4 = pic_height - 2
+		else:
+			if (a_2 -(a_4)) <= 0:
+				y_4 = 1
+			else: 
+				y_4 = a_2 -(a_4)
+
+		if (a_2 ) >= pic_height:
+			y_5 = pic_height - 2
+		else:
+			y_5 = a_2 
+
+		if (a_2  + a_4) >= pic_height:
+			y_6 = pic_height - 2
+		else:
+			y_6 = a_2  + a_4
+
+		if (a_2  + a_4) >= pic_height:
+			y_7 = pic_height - 2
+		else:
+			y_7 = a_2  + a_4
+
+		if (a_2  + a_4) >= pic_height:
+			y_8 = pic_height - 2
+		else:
+			y_8 = a_2  + a_4
+
 		string = string + '{"shape_attributes":{"name":"polygon","all_points_x":[' + str(x_1) + ',' + str(x_2) + ',' + str(x_3) + ',' + str(x_4) + ',' + str(x_5) + ',' + str(x_6) + ',' + str(x_7) + ',' + str(x_8) + '],"all_points_y":[' + str(y_1) + ',' + str(y_2) + ',' + str(y_3) + ',' + str(y_4) + ',' + str(y_5) + ',' + str(y_6) + ',' + str(y_7) + ',' + str(y_8) + ']},"region_attributes":{}}'	
 	string = string + '],"file_attributes":{}}'
 	split1 = split1 + 1
